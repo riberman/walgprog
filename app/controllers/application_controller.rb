347fb7889b7da6c72c95f2f455d6aad5ea4ca423
Diego Marczal <marczal@utfpr.edu.br>
2019-04-02 19:03:26 -0300

    Setup layout for sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
- 
+
+  layout :layout_by_resource
+
   protected
+
+  def layout_by_resource
+    return 'layouts/devise/session' if devise_controller?
+
+    'layouts/application'
+  end
 
   def after_sign_in_path_for(*)
     admins_root_path
