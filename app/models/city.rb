115729febbbd86817e5affc173791fc96aa7fed6
pedroida <pedrohsida@hotmail.com>
2019-04-16 19:34:26 -0300

    order cities

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,4 +1,6 @@
 class City < ApplicationRecord
+  default_scope {order(:name, "name DESC")}
+
   has_many :institutions, dependent: :restrict_with_error
   has_many :events, dependent: :restrict_with_error
   belongs_to :state
--- test/controllers/admins/events_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Admins::EventsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
