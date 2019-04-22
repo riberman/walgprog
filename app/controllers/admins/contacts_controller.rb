3ff593cea6a9e7d4d556b2524ee5b12e3e3ad1c4
vinicius <vinicius@pcs-MacBook-Pro-2.local>
2019-04-22 01:31:31 -0300

    added feature list all contacts

--- app/controllers/admins/contacts_controller.rb
+++ app/controllers/admins/contacts_controller.rb
@@ -1,2 +1,15 @@
-class ContactsController < ApplicationController
+class Admins::ContactsController < Admins::BaseController
+  before_action :authenticate_admin!
+
+  def index
+    @contacts = Contact.all
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
 end
