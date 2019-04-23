f76545e58e282d0df6fa4dc95fb34e6e2752a192
Peterson <peterson.petio@gmail.com>
2019-04-23 18:35:05 -0300

    tests index

---
+++ spec/factories/events.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :event do
+    sequence(:name) { |n| "Name #{n}" }
+    city
+    beginning_date { 5.years.ago }
+    end_date { 2.years.after }
+    color { '#fff' }
+    initials { 'TST' }
+    local {'Parque'}
+    address {'testing'}
+
+  end
+end
---
+++ spec/features/events/events_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'Admin:login', type: :feature do
+  let(:admin) { create(:admin) }
+  let!(:event) { create(:event) }
+
+  before(:each) do
+    login_as(admin, scope: :admin)
+    visit admins_events_path
+  end
+
+  it 'displays the events list' do
+
+  expect(page).to have_content(event.name)
+  expect(page).to have_content(I18n.l(event.beginning_date, format: :short))
+  expect(page).to have_content(I18n.l(event.end_date, format: :short))
+  expect(page).to have_content(event.local)
+  expect(page).to have_content(event.city.name)
+  expect(page).to have_content(event.address)
+
+
+  end
+end
