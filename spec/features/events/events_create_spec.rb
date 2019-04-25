855415b5e1a76b3f1897ab9862183646d40f107e
Peterson <peterson.petio@gmail.com>
2019-04-24 23:09:11 -0300

    Add event create adjustments

--- spec/features/events/events_create_spec.rb
+++ spec/features/events/events_create_spec.rb
@@ -9,25 +9,37 @@ describe 'Event:create', type: :feature do
     visit new_admins_event_path
   end
 
-  it 'create event', js: true do
+  context 'when event is not valid', js: true do
+    it 'create event' do
 
-    attributes = attributes_for(:event)
-    fill_in 'event_name',   with: attributes[:name]
-    fill_in 'event_initials',   with: attributes[:initials]
-    fill_in 'event_color',   with: attributes[:color]
-    fill_in 'event_local',   with: attributes[:local]
-    fill_in 'event_address',   with: attributes[:address]
+      attributes = attributes_for(:event)
+      fill_in 'event_name',   with: attributes[:name]
+      fill_in 'event_initials',   with: attributes[:initials]
+      fill_in 'event_color',   with: attributes[:color]
+      fill_in 'event_local',   with: attributes[:local]
+      fill_in 'event_address',   with: attributes[:address]
 
 
-    find('input[name="commit"]').click
+      find('input[name="commit"]').click
 
-    expect(page).to have_current_path admins_events_path
+      expect(page).to have_current_path admins_events_path
+      success_message = I18n.t('events.success.new')
+      expect(page).to have_flash(:success, text: success_message)
+      expect(page).to have_message(attributes[:name], in: 'table tbody')
+    end
+  end
 
-    success_message = I18n.t('events.success.new')
+  context 'when event is not valid', js: true do
+    it 'show errors' do
+      find('input[name="commit"]').click
+      expect(page).to have_flash(:danger, text: I18n.t('flash.actions.errors'))
 
-    expect(page).to have_flash(:success, text: success_message)
-
-    expect(page).to have_message(attributes[:name], in: 'table tbody')
-
+      message_blank_error = I18n.t('errors.messages.blank')
+      expect(page).to have_message(message_blank_error, in: 'div.event_name')
+      expect(page).to have_message(message_blank_error, in: 'div.event_local')
+      expect(page).to have_message(message_blank_error, in: 'div.event_color')
+      expect(page).to have_message(message_blank_error, in: 'div.event_address')
+      expect(page).to have_message(message_blank_error, in: 'div.event_initials')
+    end
   end
 end
