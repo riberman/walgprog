b1aa377d9cb1090640b09c554420fa171080d9b4
Peterson <peterson.petio@gmail.com>
2019-04-25 00:12:43 -0300

    Add adjustments on js

--- app/assets/javascripts/admins/events.js
+++ app/assets/javascripts/admins/events.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
-    $('.colorpicker').colorpicker();
+  $('.colorpicker').colorpicker();
 
-    $('.datetimepicker-input').datetimepicker({
-        locale: 'pt-br',
-        sideBySide: true,
-        autoclose: true
-    });
+  let options = {
+    locale: 'pt-br',
+    sideBySide: true,
+    autoclose: true
+  };
+
+  if (window.location.pathname.match(/new/) !== null ){
+    options.date = moment().format('YYYY-MM-DD hh:mm');
+  }
+
+  $('.datetimepicker-input').datetimepicker(options);
 });
