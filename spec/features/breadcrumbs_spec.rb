f0b6ff6aaf9048c2b41c560108cc07c39fb0a21c
milena-mognon <milenamognon@gmail.com>
2019-05-13 07:29:10 -0300

    feature test fixed

--- spec/features/breadcrumbs_spec.rb
+++ spec/features/breadcrumbs_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'Breadcrumbs', type: :feature do
-  context 'verify presence' do
+  context 'with the correct' do
     let(:admin) { create(:admin) }
 
     before(:each) do
@@ -9,24 +9,24 @@ describe 'Breadcrumbs', type: :feature do
       visit edit_admin_registration_path
     end
 
-    it "should have 3 li elements" do
-      expect(find('ol')).to have_selector('li', count: 3)
+    it 'text' do
+      i = 0
+      breadcrumbs_text = [I18n.t('breadcrumbs.homepage'), '/', I18n.t('breadcrumbs.Admins.edit')]
+      all('li').each do |li|
+        puts li.text
+        expect(li.text).to have_content(breadcrumbs_text[i])
+        i += 1
+      end
     end
 
-    it "should have text" do
-      i = -1
-      k = -1
-      paths_text = [:root_path, :edit_admin_registration_path]
-      breadcrumbs_text = [I18n.t('breadcrumbs.homepage'), "/", I18n.t('breadcrumbs.Admins.edit')]
-      breadcrumbs = all('li').each { |li| li[:text] }
-      paths = all('li a').each { |a| a[:href] }
-      breadcrumbs.each { |l|
-        messege =  l.text
-        expect(messege).to have_content(breadcrumbs_text[i+=1])
-        if breadcrumbs.count / 2 != 0
-          expect(paths[k+=2]).to have_content(paths_text[k+=2])
-        end
-      }
+    it 'url' do
+      expected_paths = ['/admins']
+      i = 0
+      all('li a').each do |a|
+        puts a[:href] == expected_paths[i]
+        expect(a[:href]).to have_content(expected_paths[i])
+        i += 1
+      end
     end
   end
 end
