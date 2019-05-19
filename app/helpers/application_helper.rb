9ed407847076bd96d2363fb2f552bd3263babdf5
Peterson <peterson.petio@gmail.com>
2019-05-19 15:13:39 -0300

    Add logo image with configs TODO: adjustments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,5 +10,11 @@ module ApplicationHelper
   def bootstrap_class_for(flash_type)
     { success: 'alert-success', error: 'alert-danger', alert: 'alert-warning',
       notice: 'alert-info' }[flash_type.to_sym] || flash_type.to_s
+    end
+    def logo_color
+      e = Event.order(end_date: :desc).first
+      return e.color if e
+
+      '#000'
+    end
   end
-end
