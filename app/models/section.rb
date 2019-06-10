5321c1f641312b0bf54f4d2ebc414634b4c042ea
pedroida <pedrohsida@hotmail.com>
2019-06-09 23:45:53 -0300

    fixing section status

--- app/models/section.rb
+++ app/models/section.rb
@@ -6,11 +6,11 @@ class Section < ApplicationRecord
   validates :title, :content, :status, :icon, :index, :event_id, presence: true
   validates :alternative_text, presence: true, if: -> { :status == 'O' }
 
-  enum status_type: { active: 'A', inactive: 'I', other: 'O' }, _prefix: :status_type
+  enum status: { active: 'A', inactive: 'I', other: 'O' }, _prefix: :status
 
   def self.human_status_types
     hash = {}
-    status_types.each { |key, value| hash[I18n.t("enums.status_types.#{key}")] = value }
+    statuses.each { |key, value| hash[I18n.t("enums.status_types.#{key}")] = value }
     hash
   end
 end
