8637d425b3ead39ced87f46e1fa2653f20384364
Patrick Ferro Ribeiro <patrick.ferro.ribeiro@gmail.com>
2019-06-17 23:23:15 -0300

    Form Contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,10 @@
 class ContactsController < ApplicationController
-  def new; end
+  def new
+    @contact = Contact.new
+  end
 
-  def create; end
+  def create
+    @contact = Contact.new(params_contact)
+    action_success? @contact.save, :new, 'flash.actions.create.m'
+  end
 end
