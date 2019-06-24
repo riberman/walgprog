ca2bdad4c1107644fcf0d30c623c0850b40b01ba
pedroida <pedrohsida@hotmail.com>
2019-06-24 19:28:07 -0300

    fixing section model tests

--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -3,30 +3,11 @@ require 'rails_helper'
 RSpec.describe Section, type: :model do
   describe 'validates' do
     [:title,
-     :content,
+     :content_markdown,
      :icon,
      :index,
      :status].each do |field|
       it { is_expected.to validate_presence_of(field) }
-    end
-
-    it 'parse markdown to html' do
-      html = <<-HTML.chomp.strip_heredoc
-        <h1>Effugiam erit cinerem tenuere concurrere</h1>
-
-        <h2>Mihi persequar et trementi muris constant tibique</h2>
-
-        <p>Lorem markdownum, abstulerunt preces prima. Ripas et concipit <strong>genuit</strong>.</p>
-
-      HTML
-
-      expect(section.content).to eql(html)
-    end
-
-    it 'create description short' do
-      short_description = "#{section.content_markdown[0...100].gsub!(/[^0-9A-Za-z]/, ' ')}..."
-
-      expect(section.description_short).to eql(short_description)
     end
 
     context 'with status' do
@@ -53,6 +34,29 @@ RSpec.describe Section, type: :model do
         section.alternative_text = nil
         expect(section).not_to be_valid
       end
+    end
+  end
+
+  describe '.content_markdown' do
+    section = create(:create)
+
+    it 'parse markdown to html' do
+      html = <<-HTML.chomp.strip_heredoc
+        <h1>Effugiam erit cinerem tenuere concurrere</h1>
+
+        <h2>Mihi persequar et trementi muris constant tibique</h2>
+
+        <p>Lorem markdownum, abstulerunt preces prima. Ripas et concipit <strong>genuit</strong>.</p>
+
+      HTML
+
+      expect(section.content).to eql(html)
+    end
+
+    it 'create description short' do
+      short_description = "#{section.content_markdown[0...100].gsub!(/[^0-9A-Za-z]/, ' ')}..."
+
+      expect(section.description_short).to eql(short_description)
     end
   end
 
