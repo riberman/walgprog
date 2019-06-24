f2ce73393145314efd2b9f707fd7942510106eee
Peterson <peterson.petio@gmail.com>
2019-06-24 11:06:42 -0300

    test

--- db/migrate/20190416221819_create_contacts.rb
+++ db/migrate/20190416221819_create_contacts.rb
@@ -5,8 +5,9 @@ class CreateContacts < ActiveRecord::Migration[5.2]
       t.string :email
       t.string :phone
       t.references :institution, foreign_key: true
-
+      #t.string :access_token
       t.timestamps
     end
+    #add_index :contacts, :access_token, unique: true
   end
 end
