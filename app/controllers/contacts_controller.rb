94b9b477aee9b7c48a6448e0bad7a6ce46dbc993
Guilherme Ribas Carneiro <guilhermecarneiro@alunos.utfpr.edu.br>
2019-06-25 18:54:20 -0300

    refactored contact controller and contact model

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,37 +3,23 @@ class ContactsController < ApplicationController
   before_action :set_contact, only: [:unregister, :update, :edit]
 
   def edit
-    final_valid_time = (@contact.update_data_send_at + 2.hours)
-    if (params[:token].eql? (@contact.update_data_token)) && (final_valid_time > Time.zone.now)
-        render :edit
+    if @contact.valid_token(params)
+      render :edit
+    else
+      flash[:error] = I18n.t('flash.actions.errors')
     end
   end
 
   def unregister
-      if params[:token].eql? @contact.unregister_token
-        if Contact.update(params[:id], unregistered: true)
-          ContactMailer.with(contacts: @contact).unregistered_contact.deliver
-          render 'contacts/contact_unregistered'
-        end
-      else
-        # apenas para teste
-        redirect_to admins_institutions_path
-      end
+    if @contact.update_by_token_to_unregister(params)
+      render 'contacts/contact_unregistered'
+    else
+      redirect_to admins_institution_path
+    end
   end
 
   def update
-    final_valid_time = (@contact.update_data_send_at + 2.hours)
-
-    if (params[:token].eql? (@contact.update_data_token)) && (final_valid_time > Time.zone.now)
-      if @contact.update(params_contact)
-        ContactMailer.with(contacts: @contact).self_update_contact.deliver
-        render 'contacts/update_success'
-      else
-        render 'edit'
-      end
-    else
-      render 'contacts/time_exceeded'
-    end
+    render @contact.update_by_token(params, params_contact)
   end
 
   private
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -21,4 +21,42 @@ class Contact < ApplicationRecord
       self[column] = SecureRandom.urlsafe_base64
     end while Contact.exists?(column => self[column])
   end
+
+  def update_by_token(params, params_contact)
+    if valid_token(params)
+      if update(params_contact)
+        ContactMailer.with(contacts: self).self_update_contact.deliver
+        return 'contacts/update_success'
+      end
+      'edit'
+    end
+    'contacts/time_exceeded'
+  end
+
+  def update_by_token_to_unregister(params)
+    if equal_token(params)
+      if Contact.update(params[:id], unregistered: true)
+        ContactMailer.with(contacts: self).unregistered_contact.deliver
+        return true
+      end
+    end
+    false
+  end
+
+  def equal_token(params)
+    return true if params[:token].eql? unregister_token
+
+    false
+  end
+
+  def valid_token(params)
+    final_valid_time = (update_data_send_at + 2.hours)
+
+    if (params[:token].eql? (update_data_token)) && (final_valid_time > Time.zone.now)
+      return true
+    end
+
+    false
+  end
+
 end
